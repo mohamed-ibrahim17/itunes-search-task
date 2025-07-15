f0915afdb86da794bcbc9d9cec4e8cc78d513d35
mohamed-ibrahim17 <engmohamed2017@hotmail.com>
2025-07-16 01:41:39 +0300

    enhance: page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { TopPodcasts } from "@/home/TopPodcasts/TopPodcasts";
 
 export default function Home() {
   return (
-    <div className="font-sans grid h-full overflow-y-auto pb-20 gap-16">
-      <main className="flex flex-col gap-[32px] items-center sm:items-start w-full overflow-x-hidden">
+    <div className="font-sans grid h-full overflow-y-auto pb-10 gap-16">
+      <main className="flex flex-col gap-y-[25px] w-full overflow-x-hidden">
         <TopPodcasts />
 
         <TopEpisodes />
