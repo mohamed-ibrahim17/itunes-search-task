4ae8fb9715feafe8ccf4785d00e9fc43d6003245
mohamed-ibrahim17 <engmohamed2017@hotmail.com>
2025-07-16 01:18:07 +0300

    refactor: create new shared components

---
+++ src/components/NoResult.tsx
@@ -0,0 +1,5 @@
+export const NoResultFound = () => (
+  <p className="text-center text-gray-400 py-10">
+    There are no results for this search.
+  </p>
+);
---
+++ src/components/StartSearching.tsx
@@ -0,0 +1,5 @@
+export const StartSearching = () => (
+  <p className="text-center text-gray-400 py-10">
+    Start searching using the search bar above.
+  </p>
+);
