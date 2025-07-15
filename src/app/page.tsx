0a66227f12425612c55b44a617939f66bc75bdbe
mohamed-ibrahim17 <engmohamed2017@hotmail.com>
2025-07-15 23:14:08 +0300

    feat: refactor page and enhance ui alignment

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { TopEpisodes } from "@/home/TopEpisodes";
+import { TopPodcasts } from "@/home/TopPodcasts";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid h-full overflow-y-auto pb-20 gap-16">
+      <main className="flex flex-col gap-[32px] items-center sm:items-start w-full overflow-x-hidden">
+        <TopPodcasts />
+
+        <TopEpisodes />
+      </main>
+    </div>
+  );
+}
